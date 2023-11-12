690032e825b3dcddf0e3a86ef52ea7bfd985336a
Emilio Ziniades <emilioziniades@protonmail.com>
2023-11-12 16:17:17 +0200

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Website that lives at https://emilio.co.za";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils/main";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = [pkgs.zola];
+          shellHook = ''
+            $SHELL
+          '';
+        };
+      }
+    );
+}
