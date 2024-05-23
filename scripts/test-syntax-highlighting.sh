84bb2a498a6ad3e9ef5dc55aa6226ba5358910cb
Emilio Ziniades <emilioziniades@protonmail.com>
2024-05-23 03:07:50 +0200

    add script to test syntax highlight themes

---
+++ scripts/test-syntax-highlighting.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+themes=$(curl -s https://api.github.com/repos/getzola/zola/contents/sublime/themes | jq -r '.[].name' | sed 's/.tmTheme//')
+
+echo "All themes:"
+echo $themes | tr " " "\n"
+echo ""
+
+for theme in $themes; do
+	echo $theme
+	sed -i "s/highlight_theme = \".*\"/highlight_theme = \"$theme\"/" config.toml
+	echo "press any key to try the next theme"
+	read
+done
