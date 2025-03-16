dc13587c1507ccea8935d486da18b7626b9b11fb
Emilio Ziniades <emilioziniades@protonmail.com>
2025-03-16 18:44:53 +0200

    sort out light/dark code block syntax highlighting

--- scripts/test-syntax-highlighting.sh
+++ scripts/test-syntax-highlighting.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-themes=$(curl -s https://api.github.com/repos/getzola/zola/contents/sublime/themes | jq -r '.[].name' | sed 's/.tmTheme//')
+themes=$(curl -s https://api.github.com/repos/getzola/zola/contents/components/config/sublime/themes | jq -r '.[].name' | sed 's/.tmTheme//')
 
 echo "All themes:"
-echo $themes | tr " " "\n"
+echo "$themes" | tr " " "\n"
 echo ""
 
 for theme in $themes; do
-	echo $theme
-	sed -i "s/highlight_theme = \".*\"/highlight_theme = \"$theme\"/" config.toml
-	echo "press any key to try the next theme"
-	read
+    echo "$theme"
+    sed -i "s/highlight_theme = \".*\"/highlight_theme = \"$theme\"/" config.toml
+    echo "press any key to try the next theme"
+    read -r
 done
